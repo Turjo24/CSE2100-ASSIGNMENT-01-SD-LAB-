902618291309a1d164f5b7068b099a991ca60bd8
Turjo24 <turjodutta023@gmail.com>
2023-12-06 21:07:22 +0600

    =main file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,406 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:assign/main.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: turjo(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class turjo extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Productdetails(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-
-}
-
-
-class Productdetails extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Details'),
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-
-              ProductCard(
-                name: 'iPhone 11 128GB ',
-                image: 'https://www.startech.com.bd/image/cache/catalog/mobile/apple/iphone-11/iphone-11-black-500x500.webp',
-                price: '৳68,500',
-                features: [
-                  'Model: iPhone 11',
-                  'Display: 6.1" Liquid Retina HD display',
-                  'Processor: A13 Bionic Chip, Storage: 128GB',
-                  'Camera: 12MP + 12MP Rear, 12MP Front'
-                ],
-              ),
-              SizedBox(height: 14),
-              ProductCard(
-                name: 'Vivo Y27e Smartphone (8/256GB)',
-                image: 'https://www.startech.com.bd/image/cache/catalog/mobile/vivo/y27e/y27e-02-500x500.webp',
-                price: '৳34,000',
-                features: [
-                  'Model: Y27e',
-                  'Display: 6.62" FHD+120Hz Colour-Rich Display',
-                  'Processor: MediaTek Helio G99 (6 nm)',
-                ],
-              ),
-
-            ],
-          ),
-        ),
-      ),
-  backgroundColor: Colors.yellow,  );
-  }
-}
-
-class ProductCard extends StatelessWidget {
-  final String name;
-  final String image;
-  final String price;
-  final List<String> features;
-
-  ProductCard({
-    required this.name,
-    required this.image,
-    required this.price,
-    required this.features,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          color: Colors.red.shade400,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Image.network(
-                image,
-                height: 100,
-                width: 100,
-                fit: BoxFit.contain,
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Price: $price',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Key Features:',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: features.map((feature) => Text('- $feature')).toList(),
-              ),
-              SizedBox(height: 16),
-              Container(
-                child: ElevatedButton(
-
-                  onPressed: () {
-                    Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
-                    // Implement your Buy Now action
-                  },
-                  child: Container(
-                    color: Colors.amber,
-                      child: Text('Buy Now',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),)),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BuyNowPage(),
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'turjo'),
     );
   }
 }
 
-class BuyNowPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  _BuyNowPageState createState() => _BuyNowPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _BuyNowPageState extends State<BuyNowPage> {
-  String selectedPaymentMethod = 'BKash'; // Default selection
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Buy Now'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
             Text(
-              'Choose Payment Method',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-
-              ),
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-
-
-
-
-
-            SizedBox(height: 16),
-             Container(
-               color: Colors.grey,
-               child: Padding(
-                 padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10.0),
-                        child: BkashPaymentMethodButton(
-                          method: 'BKash',
-                          onPressed: () => selectPaymentMethod('BKash'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10.0),
-                        child: rocketPaymentMethodButton(
-                          method: 'Rocket',
-                          onPressed: () => selectPaymentMethod('Rocket'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: visaPaymentMethodButton(
-                          method: 'Visa',
-                          onPressed: () => selectPaymentMethod('Visa'),
-                        ),
-                      ),
-                    ],
-                  ),
-
-               ),
-             ),
-
-            Container(
-              color: Colors.grey,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SingleChildScrollView(
-
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left:1.0),
-                            child: mcPaymentMethodButton(
-                              method: 'Mastercard',
-                              onPressed: () => selectPaymentMethod('Mastercard'),
-                            ),
-                          ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(left:20.0),
-                        child: amexPaymentMethodButton(
-                          method: 'AmEx',
-                          onPressed: () => selectPaymentMethod('AmEx'),
-                        ),
-                      ),
-
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-
-
-            SizedBox(height: 100),
-            Center(
-              child: DropdownButton<String>(
-                value: selectedPaymentMethod,
-                items: ['BKash', 'Rocket', 'Visa', 'Mastercard', 'AmEx']
-                    .map((method) => DropdownMenuItem<String>(
-                  value: method,
-                  child: Text(method),
-                ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    selectedPaymentMethod = value!;
-                  });
-                },
-              ),
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.only(top: 200.0),
-              child: SingleChildScrollView(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,MaterialPageRoute(builder: (context) => turjo()));
-                  },
-                  child: Container(
-                      child: Text('Go Back',style: TextStyle(color: Colors.yellow,fontWeight: FontWeight.bold),)),
-                ),
-              ),
-            ),
-
           ],
         ),
       ),
-    backgroundColor: Colors.cyan,);
-  }
-
-  void selectPaymentMethod(String method) {
-    setState(() {
-      selectedPaymentMethod = method;
-    });
-  }
-}
-
-class BkashPaymentMethodButton extends StatelessWidget {
-  final String method;
-  final VoidCallback onPressed;
-
-  BkashPaymentMethodButton({required this.method, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Image.asset(
-        'lib/assets/Bkash.png',
-
-        height: 70,
-        width: 70,
-      ),
-    );
-  }
-}
-
-class rocketPaymentMethodButton extends StatelessWidget {
-  final String method;
-  final VoidCallback onPressed;
-
-  rocketPaymentMethodButton({required this.method, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Image.asset(
-        'lib/assets/rocket.png',
-
-        height: 70,
-        width: 70,
-      ),
-    );
-  }
-}
-class visaPaymentMethodButton extends StatelessWidget {
-  final String method;
-  final VoidCallback onPressed;
-
-  visaPaymentMethodButton({required this.method, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Image.asset(
-        'lib/assets/visa.png',
-
-        height: 70,
-        width: 70,
-      ),
-    );
-  }
-}
-
-
-class mcPaymentMethodButton extends StatelessWidget {
-  final String method;
-  final VoidCallback onPressed;
-
-  mcPaymentMethodButton({required this.method, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Image.asset(
-        'lib/assets/mastercard.png',
-
-        height: 70,
-        width: 70,
-      ),
-    );
-  }
-}
-
-
-
-class amexPaymentMethodButton extends StatelessWidget {
-  final String method;
-  final VoidCallback onPressed;
-
-  amexPaymentMethodButton({required this.method, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Image.asset(
-        'lib/assets/amex.png',
-
-        height: 70,
-        width: 70,
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
